6eb87e16881752eeb7c132a64337ea0d423ddff1
piximeow <annapolishchuk2015@gmail.com>
2025-10-12 15:35:27 +0300

    Delete 3.js

--- 3.js
+++
@@ -1,6 +0,0 @@
-const mas1 = [true, 'hello', 5, 12, -200, false, false, 'word', 100, true, 'bye', 10, 5, 1, 'lalal', 'Ukraine', true]
-const hash1 = { number: 0, string: 0, boolean: 0 }
-for (const item of mas1) {
-    hash1[typeof item] += 1
-}
-console.log(hash1)
