25f757bb62b2f3adafcd2e6638026726969b20a5
piximeow <annapolishchuk2015@gmail.com>
2025-10-12 16:04:42 +0300

    3.js

--- lab1/3.js
+++ lab1/3.js
@@ -1,6 +1,7 @@
-const mas1 = [true, 'hello', 5, 12, -200, false, false, 'word', 100, true, 'bye', 10, 5, 1, 'lalal', 'Ukraine', true]
+const mas1 = [true, 'hello', 5, 12, -200, false, false, 'word', 100, true, 'bye', 10, 5, 1, '1707', 'sleep', true]
 const hash1 = { number: 0, string: 0, boolean: 0 }
 for (const item of mas1) {
     hash1[typeof item] += 1
 }
+
 console.log(hash1)
